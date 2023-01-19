f2ca8214d3377e2afcab6c0ae5fc6dca18db17d3
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2023-01-19 18:43:38 +0600

    feat:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,8 @@ import { ApolloProvider } from '@apollo/client';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import ProgressBar from '@badrap/bar-of-progress';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-
 import { useApollo } from '~/lib/apolloClient';
 import { wrapper } from '~/redux/store';
-
 import ModalProvider from '~/contexts/ModalContext';
 import SocketProvider from '~/contexts/SocketContext';
 
@@ -26,7 +24,7 @@ Router.events.on('routeChangeStart', progressBar.start);
 Router.events.on('routeChangeComplete', progressBar.finish);
 Router.events.on('routeChangeError', progressBar.finish);
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: any) {
   const apolloClient = useApollo(pageProps);
 
   return (
