6100e1e78df11c6cd91769219a22924bcc40d15f
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2023-01-19 20:03:57 +0600

    change some code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,8 +7,7 @@ export * from './utils';
 export * from './limits';
 
 export const MONGODB_URI = process.env.MONGODB_URL as string;
+export const DOMAIN = "https://sm-instagram.vercel.app"
 
-export const FACEBOOK_CLIENT_ID = process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID;
-export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
-export const DOMAIN = __prod__ ? process.env.NEXT_PUBLIC_DOMAIN : 'http://localhost:3000';
+// __prod__ ? process.env.NEXT_PUBLIC_DOMAIN : 'http://localhost:3000';
