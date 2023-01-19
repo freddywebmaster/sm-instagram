10192c517898c274ca5691109f40c910bc12cd9c
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2023-01-19 14:21:41 +0600

    refactor:

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,4 +11,4 @@ export const MONGODB_URI = process.env.MONGODB_URL as string;
 export const FACEBOOK_CLIENT_ID = process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID;
 export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
-export const DOMAIN = __prod__ ? process.env.NEXT_PUBLIC_DOMAIN : 'http://localhost:3000';
+export const DOMAIN = __prod__ ? 'https://sm-instagram.vercel.app' : 'http://localhost:3000';
